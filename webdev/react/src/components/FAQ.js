eb9fd30074b373bc5c9f5df4e13582a176501121
consultant-github <58916728+consultant-github@users.noreply.github.com>
2022-12-11 18:03:42 -0500

    Update FAQ.js

--- webdev/react/src/components/FAQ.js
+++ webdev/react/src/components/FAQ.js
@@ -4,7 +4,7 @@ function FAQ(props) {
     return (
         <div>
             <h3 style={{display: !props.question && "none"}}>Question: {props.question}</h3>
-            <h3>Answer: {props.punchLine}</h3>
+            <h3>Answer: {props.answer}</h3>
             <hr/>
         </div>
     )
