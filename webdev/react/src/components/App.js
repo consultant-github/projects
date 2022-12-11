27602e762784c081c96947f03b2426c4149f4c9c
consultant-github <58916728+consultant-github@users.noreply.github.com>
2022-12-11 18:02:47 -0500

    Create App.js

---
+++ webdev/react/src/components/App.js
@@ -0,0 +1,27 @@
+import React from "react"
+
+import FAQ from "./FAQ"
+
+function App() {
+    return (
+        <div>
+            <FAQ
+                question="FAQ # 1?" 
+                answer="Answer # 1"
+            />
+            
+            <FAQ
+                question="FAQ # 1?" 
+                answer="Answer # 1"
+            />
+            
+            <FAQ
+                question="FAQ # 1?" 
+                answer="Answer # 1"
+            />
+                    
+        </div>
+    )
+}
+
+export default App
