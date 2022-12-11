d864b0bcb2ad183ab1edf46e42caf064aee5d936
consultant-github <58916728+consultant-github@users.noreply.github.com>
2022-12-11 17:49:11 -0500

    Update nft-erc721.sol

--- bc-nft-erc721/src/nft-erc721.sol
+++ bc-nft-erc721/src/nft-erc721.sol
@@ -1,1 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract Colors is ERC721  (
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIDCounter;
+
+    constructor() ERC721("Colors", "ESS") {}
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://TBD/api/erc721/";
+    }
+
+    function mint (address to) public returns (uint256) {
+        require(_tokenIDCounter.current() < 3);
+        _tokenIDCounter.increment();
+        _safeMint(to, _tokenIDCounter.current());
+
+        return _tokenIDCounter.current();
+    }
+)
